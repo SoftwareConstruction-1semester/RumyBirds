640e778365f8cee6de616a5f0f634bbcf0f6dca0
ev <ev@evang.dk>
2014-10-02 10:34:20 +0200

    adds project to git

---
+++ BIrdsApp/Model/BirdModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIrdsApp.Model
+{
+    class BirdModel
+    {
+        private string _color;
+        private string _origin;
+        private int _size;
+        private string _species;
+
+        public String Species
+        {
+            get { return _species; }
+            set { _species = value; }
+        }
+
+        public String Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+        public String Origin
+        {
+            get { return _origin; }
+            set { _origin = value; }
+        }
+
+        public int Size
+        {
+            get { return _size; }
+            set { _size = value; }
+        }
+    }
+}
---
+++ BIrdsApp/viewmodel/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using BIrdsApp.Annotations;
+using BIrdsApp.Model;
+
+namespace BIrdsApp.viewmodel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private BirdModel _birdOne;
+        private BirdModel _birdTwo;
+
+        public MainViewModel()
+        {
+            _birdOne = new BirdModel();
+            _birdOne.Species = "Red Lory";
+            _birdOne.Color = "Red";
+            _birdOne.Origin = "Indonesia";
+            _birdOne.Size = 31;
+
+            _birdTwo = new BirdModel();
+            _birdTwo.Species = "gossy l";
+            _birdTwo.Color = "Green mostly";
+            _birdTwo.Origin = "China";
+            _birdTwo.Size = 700;
+        }
+
+        public BirdModel BirdOne
+        {
+            get { return _birdOne; }
+            set { _birdOne = value; }
+        }
+
+        public BirdModel BirdTwo
+        {
+            get { return _birdTwo; }
+            set { _birdTwo = value; }
+        }
+        
+
+
+        #region implementation of notifyprop....
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
